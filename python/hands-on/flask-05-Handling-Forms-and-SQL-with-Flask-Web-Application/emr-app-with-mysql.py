030c6ffed747ed4bf3f959c7cfc42d671f28698b
Emrah <M.emrah.tekin@gmail.com>
2021-04-11 23:01:47 +0300

    mysql update

--- python/hands-on/flask-05-Handling-Forms-and-SQL-with-Flask-Web-Application/emr-app-with-mysql.py
+++ python/hands-on/flask-05-Handling-Forms-and-SQL-with-Flask-Web-Application/emr-app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure sqlite database
-app.config['MYSQL_DATABASE_HOST'] = 'flask-mysql-database.cpx5vhrkiml2.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'database-1.cpx5vhrkiml2.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
